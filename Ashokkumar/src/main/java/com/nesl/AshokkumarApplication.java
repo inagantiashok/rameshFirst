3c670b6548e6dea480374c06b5f7a0c38cd174fb
inagantiashok <inaganti4u@gmail.com>
2021-07-06 12:07:17 +0530

    my firstww

---
+++ Ashokkumar/src/main/java/com/nesl/AshokkumarApplication.java
@@ -0,0 +1,13 @@
+package com.nesl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshokkumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshokkumarApplication.class, args);
+	}
+
+}
